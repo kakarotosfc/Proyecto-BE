c8e9cb9c589d71f6a72ddf5e76f80b0899b5b66f
unknown <amilkir@gmail.com>
2023-04-03 23:33:14 -0300

    prueba commit 3

--- src/main/java/com/practica/hellospringboot/HellospringbootApplication.java
+++ src/main/java/com/practica/hellospringboot/HellospringbootApplication.java
@@ -15,7 +15,7 @@ public class HellospringbootApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 

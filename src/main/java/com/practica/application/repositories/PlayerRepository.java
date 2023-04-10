25fc29a62f3b2c292cca66b6d02a7f9df54be039
Amilkir <114262594+Amilkir@users.noreply.github.com>
2023-04-10 20:08:43 -0300

    Devplayer (#5)
    * adding player flow
    
    * adding player flow
    
    * adding season flow (#4)
    
    done

---
+++ src/main/java/com/practica/application/repositories/PlayerRepository.java
@@ -0,0 +1,10 @@
+package com.practica.application.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.practica.application.persistence.models.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}

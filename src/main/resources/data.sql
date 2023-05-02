1d6c735746b967fcf08d0f5054142763f926b306
unknown <amilkir@gmail.com>
2023-05-02 11:56:18 -0300

    adding data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,44 @@
+CREATE SCHEMA IF NOT EXISTS kakarotos;
+USE kakarotos;
+
+CREATE TABLE IF NOT EXISTS player_data (
+  id_file INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  number INT NOT NULL,
+  position VARCHAR(50) NULL,
+  foot CHAR NULL,
+  weak_foot INT NULL,
+  skills INT NULL,
+  start_date DATE NOT NULL,
+  departure_date DATE NULL,
+  image_url VARCHAR(255) NULL,
+  PRIMARY KEY (id_file)
+);
+
+CREATE TABLE IF NOT EXISTS season (
+  season VARCHAR(50) NOT NULL,
+  start_date DATE NULL,
+  end_date DATE NULL,
+  PRIMARY KEY (season)
+);
+
+CREATE TABLE IF NOT EXISTS player_performance (
+  player_id INT NOT NULL,
+  season VARCHAR(50) NOT NULL,
+  matches INT NOT NULL,
+  goals INT NOT NULL,
+  assists INT NOT NULL,
+  yellow_cards INT NOT NULL,
+  blue_cards INT NOT NULL,
+  red_cards INT NOT NULL,
+  mvp_matches INT NOT NULL,
+    FOREIGN KEY (player_id)
+    REFERENCES player_data (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_player_performance_season
+    FOREIGN KEY (season)
+    REFERENCES season (season)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);

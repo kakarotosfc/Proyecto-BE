b1cb5b17a830540a9a2aba8da890dc44ae850df3
Amilkir <114262594+Amilkir@users.noreply.github.com>
2023-05-20 11:31:14 -0300

    Devplayerperf (#9)
    * Add player performance flow
    
    * player performance updates
    
    * finishing playerperformance flow
    
    * adding name validation to avoid repeated players
    
    * Adding season validation to avoid duplicate registers
    
    * Modifying season and player flows
    
    * Teamperf (#8)
    
    * adding team performance flow
    
    * Modifying season and player flows
    
    * adding team performance flow
    
    * removing Embeddable label on SeasonService
    
    * saving last team performance modifications

---
+++ src/main/java/com/practica/application/services/TeamPerformanceService.java
@@ -0,0 +1,35 @@
+package com.practica.application.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practica.application.persistence.models.TeamPerformance;
+import com.practica.application.repositories.TeamPerformanceRepository;
+
+@Service
+public class TeamPerformanceService {
+    
+    @Autowired
+    private TeamPerformanceRepository teamPerformanceRepository;
+
+    public void save(TeamPerformance teamPerformance) {
+        teamPerformanceRepository.save(teamPerformance);            
+        }
+        
+ 
+    public List<TeamPerformance> list() {
+        return teamPerformanceRepository.findAll();
+    }
+
+    public TeamPerformance find(String season) {
+        Optional<TeamPerformance> seasonOptional = teamPerformanceRepository.findById(season);
+
+        if(seasonOptional.isEmpty()){
+            return null;
+        }
+        return seasonOptional.get();            
+    }
+}

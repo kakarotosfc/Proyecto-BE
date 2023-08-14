61b6ddefc30a2c5d20e8738c0c931b99c9ca61b0
WYwZTc3ZmI <luisvivas2504@gmail.com>
2023-08-14 20:27:16 -0300

    adding interceptor

---
+++ src/main/java/com/practica/application/configs/WebConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CustomInterceptor customInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(customInterceptor);
+    }
+}

fd328afae25038c0250bf3ee9497d7d546592e6b
unknown <amilkir@gmail.com>
2023-08-14 20:29:14 -0300

    adding security to endpoints

---
+++ src/main/java/com/practica/application/services/AuthService.java
@@ -0,0 +1,35 @@
+package com.practica.application.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.stereotype.Service;
+
+import com.practica.application.exceptions.DataSourceException;
+import com.practica.application.exceptions.DataSourceNotFoundException;
+import com.practica.application.persistence.models.Season;
+import com.practica.application.repositories.SeasonRepository;
+
+@Service
+public class SeasonService {
+    @Autowired
+    private SeasonRepository seasonRepository;
+    private static final String EXCEPTION_RESPONSE = "There are no Seasons created to be shown.";
+
+    public void save(Season season) {
+        try {
+            seasonRepository.save(season);
+        } catch(NestedRuntimeException ex) {
+            throw new DataSourceException(ex.getRootCause().getLocalizedMessage());
+        } 
+    }
+
+    public List<Season> list() {
+        List<Season> allSeasons = seasonRepository.findAll();
+       
+        if(allSeasons.isEmpty()) throw new DataSourceNotFoundException(EXCEPTION_RESPONSE);
+        
+        return allSeasons;            
+    }
+}
